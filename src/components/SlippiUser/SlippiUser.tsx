2c97a4dcf5567e2cc8ddb4ee792fb681f0ac45f2
Dylan Pizzo <dylan@dylanpizzo.email>
2025-07-29 20:47:35 -0700

    leaderboard page

--- src/components/SlippiUser/SlippiUser.tsx
+++ src/components/SlippiUser/SlippiUser.tsx
@@ -1,18 +1,17 @@
 import { getUserData, type UserData } from "@/util/getUserData";
 import { getRank } from "@/util/SlippiStatic";
+import styles from "./SlippiUser.module.css";
 
 export default async function SlippiUser(props: {
 	userCode: string;
 	userTag: string;
 }) {
 	const userData = await getUserData(props.userCode, props.userTag);
-	console.log(userData);
 	const top = (userData.dailyGlobalPlacement ?? 0) <= 300;
 	const rankInfo = getRank(userData.elo, top, userData.totalSets);
-	console.log(rankInfo);
 	return (
 		<a
-			className={`player-row`}
+			className={styles["player-row"] + " " + styles.a}
 			style={
 				{
 					"--rank-color": rankInfo.color,
@@ -20,44 +19,48 @@ export default async function SlippiUser(props: {
 			}
 			href={`https://slippi.gg/user/${userData.code.replace("#", "-")}`}
 		>
-			<div className="rating-container">
-				<div className="rating-image">
+			<div className={styles["rating-container"]}>
+				<div className={styles["rating-image"]}>
 					<img
 						src={`${rankInfo.image}`}
 						alt={`${rankInfo.name}`}
 					></img>
 				</div>
-				<span className="elo">{userData.elo.toFixed(1)}</span>
+				<span className={styles.elo}>{userData.elo.toFixed(1)}</span>
 			</div>
-			<div className="name-container">
-				<div className="player-tag">{userData.tag}</div>
-				<div className="slippi-player">
+			<div className={styles["name-container"]}>
+				<div className={styles["player-tag"]}>{userData.tag}</div>
+				<div className={styles["slippi-player"]}>
 					{userData.displayName} ({userData.code})
 				</div>
 			</div>
-			<div className="right">
-				<div className="win-loss">
-					<span className="wins">{userData.wins ?? 0}</span> /{" "}
-					<span className="losses">{userData.losses ?? 0}</span>
+			<div className={styles.right}>
+				<div className={styles["win-loss"]}>
+					<span className={styles.wins}>{userData.wins ?? 0}</span> /{" "}
+					<span className={styles.losses}>
+						{userData.losses ?? 0}
+					</span>
 				</div>
 				<span
-					className={`nessiness ${
-						userData.nessPercent === null
-							? "nessiness-null"
+					className={
+						styles.nessiness +
+						" " +
+						(userData.nessPercent === null
+							? styles["nessiness-null"]
 							: userData.nessPercent < 0.5
-							? "nessiness-bad"
+							? styles["nessiness-bad"]
 							: userData.nessPercent < 0.8
-							? "nessiness-okay"
+							? styles["nessiness-okay"]
 							: userData.nessPercent < 1
-							? "nessiness-good"
-							: ""
-					}`}
+							? styles["nessiness-good"]
+							: "")
+					}
 				>
 					{userData.nessPercent === null
 						? ""
 						: (userData.nessPercent * 100).toFixed(0) + "%"}
 					<img
-						className="nessicon"
+						className={styles.nessicon}
 						src="https://slippi.gg/images/characters/stock-icon-11-0.png"
 					></img>
 				</span>
