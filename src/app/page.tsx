671e24423e6c400ea15f4de7021cf37bbbd7855a
Dylan Pizzo <dylan@dylanpizzo.email>
2025-08-27 19:54:29 -0700

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import { createClient } from "@/util/supabase/server";
+import { redirect } from "next/navigation";
+// import { createClient } from "@/util/supabase/server";
 
 export default async function Home() {
-	const supabase = await createClient();
-	const { data: pages } = await supabase.from("pages").select();
+	redirect(`/leaderboard`);
+	// const supabase = await createClient();
+	// const { data: pages } = await supabase.from("pages").select();
 
-	return <pre>{JSON.stringify(pages, null, 2)}</pre>;
+	// return <pre>{JSON.stringify(pages, null, 2)}</pre>;
 }
