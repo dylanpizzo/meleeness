1e2e2510ab9a2c0bc7d358693a91a34cf5e36ad2
Dylan Pizzo <dylan@dylanpizzo.email>
2025-08-27 21:02:42 -0700

    Add ability to save users

---
+++ src/app/api/slippi/user-data/route.ts
@@ -0,0 +1,23 @@
+import { getRawSlippiData } from "@/util/getRawSlippiData";
+import { createMasterClient } from "@/util/supabase/master";
+
+export async function GET() {
+	const supabase = await createMasterClient();
+	const { data: userRows } = await supabase.from("slippi_users").select();
+
+	if (!userRows) {
+		return Response.json({ error: "Couldn't get user rows." });
+	}
+
+	await Promise.all(
+		userRows.map(async (userRow) => {
+			const slippiData = await getRawSlippiData(userRow.connect_code);
+			return await supabase.from("slippi_user_data").insert({
+				slippi_user: userRow.id,
+				slippi_data: slippiData,
+			});
+		})
+	);
+
+	return Response.json({ done: true, userRows });
+}
