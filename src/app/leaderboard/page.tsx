466dd17d8fb02bfcc9e3b847f684ff6bbb375ba3
Dylan Pizzo <dylan@dylanpizzo.email>
2025-08-30 00:31:49 -0700

    add title, footer, and opengraph

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -6,6 +6,13 @@ import { formatUserData } from "@/util/formatUserData";
 
 export const metadata: Metadata = {
 	title: "Ness Leaderboard",
+	openGraph: {
+		type: "website",
+		url: "https://meleeness.com/leaderboard",
+		title: "Ness Leaderboard",
+		description: "A leaderboard for Slippi Nesses. Who's the best Ness?",
+		images: [{ url: "https://meleeness.com/assets/nessrank.png" }],
+	},
 };
 
 // The contents depend on dynamic values
@@ -24,11 +31,28 @@ export default async function Leaderboard() {
 		.sort((a, b) => b.elo - a.elo);
 	return (
 		<div className={styles.page}>
+			<div className={styles.title}>
+				<img
+					className={styles.nessicon}
+					src="https://slippi.gg/images/characters/stock-icon-11-0.png"
+				></img>{" "}
+				Ness Leaderboard
+			</div>
 			<main className={styles.main}>
 				{allData.map((userData) => (
 					<SlippiUser key={userData.code} userData={userData} />
 				))}
 			</main>
+			<div className={styles.footer}>
+				Join the{" "}
+				<a
+					className={styles.link}
+					href="https://discord.gg/011MSaTVv85tRjQWO"
+				>
+					Nesscord
+				</a>{" "}
+				for more Ness!
+			</div>
 		</div>
 	);
 }
