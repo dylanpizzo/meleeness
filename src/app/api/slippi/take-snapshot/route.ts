6aedb67debf99e5ed29c9db724f0bcb334dff0d0
Dylan Pizzo <dylan@dylanpizzo.email>
2025-09-04 20:31:36 -0700

    Only save new slippi data

--- src/app/api/slippi/take-snapshot/route.ts
+++ src/app/api/slippi/take-snapshot/route.ts
@@ -1,10 +1,13 @@
 import { getRawSlippiData } from "@/util/getRawSlippiData";
+import { getSavedSlippiData } from "@/util/getSavedSlippiData";
 import { createMasterClient } from "@/util/supabase/master";
 
 // The contents depend on dynamic values
 export const dynamic = "force-dynamic";
 
 export async function GET() {
+	const savedSlippiData = await getSavedSlippiData();
+
 	const supabase = await createMasterClient();
 	const { data: userRows } = await supabase.from("slippi_users").select();
 
@@ -15,10 +18,23 @@ export async function GET() {
 	await Promise.all(
 		userRows.map(async (userRow) => {
 			const slippiData = await getRawSlippiData(userRow.connect_code);
-			return await supabase.from("slippi_user_data").insert({
-				slippi_user: userRow.id,
-				slippi_data: slippiData,
-			});
+			const prevSlippiData = savedSlippiData.find(
+				(x) => x.getUser.connectCode === slippiData.getUser.connectCode
+			);
+			const oldSlippiProfile =
+				prevSlippiData?.getUser?.rankedNetplayProfile;
+			const newSlippiProfile = slippiData.getUser.rankedNetplayProfile;
+
+			if (
+				!oldSlippiProfile ||
+				oldSlippiProfile.ratingUpdateCount !==
+					newSlippiProfile.ratingUpdateCount
+			) {
+				return await supabase.from("slippi_user_data").insert({
+					slippi_user: userRow.id,
+					slippi_data: slippiData,
+				});
+			}
 		})
 	);
 
